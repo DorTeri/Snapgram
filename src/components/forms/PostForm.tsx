1fb7c81dcc19f5ce4ae43e895e7cd210a52cc889
DorTeri <dortayari@gmail.com>
2023-11-13 16:25:08 +0200

    Added create post function and keys

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -1,39 +1,52 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "../ui/textarea"
 import { Button } from "../ui/button"
 import FileUploader from "../shared/FileUploader"
+import { PostValidation } from "@/lib/validation"
+import { Models } from "appwrite"
+import { useUserContext } from "@/context/AuthContext"
+import { useToast } from "../ui/use-toast"
+import { useNavigate } from "react-router-dom"
+import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-})
+type PostFormProps = {
+    post?: Models.Document
+}
 
-const PostForm = ({post}) => {
+const PostForm = ({ post }: PostFormProps) => {
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost()
+    const { user } = useUserContext();
+    const { toast } = useToast()
+    const navigate = useNavigate()
+
+    const form = useForm<z.infer<typeof PostValidation>>({
+        resolver: zodResolver(PostValidation),
         defaultValues: {
-            username: "",
-        },
+            caption: post ? post?.caption : '',
+            file: [],
+            location: post ? post?.location : '',
+            tags: post ? post?.tags.join(',') : ''
+        }
     })
 
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    async function onSubmit(values: z.infer<typeof PostValidation>) {
+        const newPost = await createPost({
+            ...values,
+            userId: user.id,
+        })
+
+        if (!newPost) {
+            toast({
+                title: 'Please try again'
+            })
+        }
+
+        navigate('/')
     }
 
     return (
@@ -59,9 +72,9 @@ const PostForm = ({post}) => {
                         <FormItem>
                             <FormLabel className="shad-form_label">Add Photos</FormLabel>
                             <FormControl>
-                                <FileUploader 
-                                fieldChange={field.onChange}
-                                mediaUrl={post}
+                                <FileUploader
+                                    fieldChange={field.onChange}
+                                    mediaUrl={post?.imageUrl}
                                 />
                             </FormControl>
                             <FormMessage className="shad-form_message" />
@@ -75,7 +88,7 @@ const PostForm = ({post}) => {
                         <FormItem>
                             <FormLabel className="shad-form_label">Add Location</FormLabel>
                             <FormControl>
-                                <Input type="text" className="shad-input" />
+                                <Input type="text" className="shad-input" {...field} />
                             </FormControl>
                             <FormMessage className="shad-form_message" />
                         </FormItem>
@@ -88,7 +101,7 @@ const PostForm = ({post}) => {
                         <FormItem>
                             <FormLabel className="shad-form_label">Add Tags (seperated by comma " , ")</FormLabel>
                             <FormControl>
-                                <Input type="text" placeholder="Art , Expression , Learn" className="shad-input" />
+                                <Input type="text" placeholder="Art , Expression , Learn" className="shad-input" {...field} />
                             </FormControl>
                             <FormMessage className="shad-form_message" />
                         </FormItem>
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { ID, Query } from "appwrite";
-import { INewUser } from "@/types";
-import { account, appwriteConfig, avatars, databases } from "./config";
+import { INewPost, INewUser } from "@/types";
+import { account, appwriteConfig, avatars, databases, storage } from "./config";
 
 export async function createUserAccount(user: INewUser) {
     try {
@@ -48,15 +48,15 @@ export async function saveUserToDB(user: {
         )
 
         return newUser
-        
+
     } catch (error) {
         console.log('saveUserToDB error', error)
     }
 }
 
-export async function signInAccount(user: {email: string; password: string;}) {
+export async function signInAccount(user: { email: string; password: string; }) {
     try {
-        const session = await account.createEmailSession(user.email , user.password)
+        const session = await account.createEmailSession(user.email, user.password)
 
         return session
     } catch (error) {
@@ -68,15 +68,15 @@ export async function getCurrentUser() {
     try {
         const currentAccount = await account.get();
 
-        if(!currentAccount) throw Error;
+        if (!currentAccount) throw Error;
 
         const currentUser = await databases.listDocuments(
             appwriteConfig.databaseId,
             appwriteConfig.userCollectionId,
-            [Query.equal('accountId' , currentAccount.$id)]
+            [Query.equal('accountId', currentAccount.$id)]
         )
 
-        if(!currentUser) throw Error;
+        if (!currentUser) throw Error;
 
         return currentUser.documents[0]
     } catch (error) {
@@ -91,5 +91,93 @@ export async function signOutAccount() {
         return session
     } catch (error) {
         console.log('signOutAccount error', signOutAccount)
+    }
+}
+
+export async function createPost(post: INewPost) {
+    try {
+        // Upload image to storage
+        const uploadedFile = await uploadFile(post.file[0])
+
+        if (!uploadedFile) throw Error;
+
+        const fileUrl = getFilePreview(uploadedFile.$id)
+
+        if (!fileUrl) {
+            deleteFile(uploadedFile.$id)
+            throw Error;
+        }
+
+        // Convert tags into an array
+
+        const tags = post.tags?.replace(/  /g, '').split(',') || []
+
+        // Save post to database
+
+        const newPost = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.postCollectionId,
+            ID.unique(),
+            {
+                creator: post.userId,
+                caption: post.caption,
+                imageUrl: uploadedFile.$id,
+                location: post.location,
+                tags: tags
+            }
+        )
+
+        if (!newPost) {
+            await deleteFile(uploadedFile.$id)
+            throw Error
+        }
+
+        return newPost
+
+    } catch (error) {
+        console.log('createPost error', error)
+    }
+}
+
+export async function uploadFile(file: File) {
+    try {
+        const uploadedFile = await storage.createFile(
+            appwriteConfig.storageId,
+            ID.unique(),
+            file
+        )
+        return uploadedFile
+    } catch (error) {
+        console.log('uploadFile error', error)
+    }
+}
+
+export async function getFilePreview(fileId: string) {
+    try {
+        const fileUrl = storage.getFilePreview(
+            appwriteConfig.storageId,
+            fileId,
+            2000,
+            2000,
+            "top",
+            100
+        )
+
+        return fileUrl
+    } catch (error) {
+        console.log('getFilePreview error', error)
+    }
+}
+
+export async function deleteFile(fileId: string) {
+    try {
+        await storage.deleteFile(
+            appwriteConfig.storageId,
+            fileId
+        )
+
+        return { status: 'ok' }
+    } catch (error) {
+        console.log('deleteFile error', error)
     }
 }
