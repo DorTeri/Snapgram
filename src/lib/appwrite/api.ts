4a7cc5792bfa81ee10384a8e7306afc0ca52ef21
DorTeri <dortayari@gmail.com>
2023-11-14 10:35:04 +0200

    Added debounce hook

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -336,3 +336,42 @@ export async function deletePost(postId: string, imageId: string) {
         console.log('deletePost error', error)
     }
 }
+
+export async function getInfinitePosts({ pageParam }: { pageParam: number }) {
+    const queries: any[] = [Query.orderDesc('$updatedAt'), Query.limit(10)]
+
+    if (pageParam) {
+        queries.push(Query.cursorAfter(pageParam.toString()))
+    }
+
+    try {
+        const posts = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.postCollectionId,
+            queries
+        )
+
+        if (!posts) throw Error
+
+        return posts
+    } catch (error) {
+        console.log('getInfinitePosts error', error)
+    }
+}
+
+export async function searchPosts(searchTerm: string) {
+
+    try {
+        const posts = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.postCollectionId,
+            [Query.search('caption', searchTerm)]
+        )
+
+        if (!posts) throw Error
+
+        return posts
+    } catch (error) {
+        console.log('getInfinitePosts error', error)
+    }
+}
